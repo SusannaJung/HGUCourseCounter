7a4e08570c240e37ab5b4598344b8f104ce5fd64
susannajung <susannajung@172.17.152.175>
2019-05-26 23:42:48 +0900

    some changes

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -10,9 +10,8 @@ import edu.handong.analysis.datamodel.Student;
 import edu.handong.analysise.utils.NotEnoughArgumentException;
 import edu.handong.analysise.utils.Utils;
 
-public class HGUCoursePatternAnalyzer
-{
- 
+public class HGUCoursePatternAnalyzer {
+
 	private HashMap<String,Student> students;
 	
 	/**
@@ -20,16 +19,13 @@ public class HGUCoursePatternAnalyzer
 	 * Run method must not be changed!!
 	 * @param args
 	 */
-	public void run(String[] args) 
-	{
+	public void run(String[] args) {
 		
-		try 
-		{
+		try {
 			// when there are not enough arguments from CLI, it throws the NotEnoughArgmentException which must be defined by you.
 			if(args.length<2)
 				throw new NotEnoughArgumentException();
-		} catch (NotEnoughArgumentException e) 
-		{
+		} catch (NotEnoughArgumentException e) {
 			System.out.println(e.getMessage());
 			System.exit(0);
 		}
@@ -37,20 +33,19 @@ public class HGUCoursePatternAnalyzer
 		String dataPath = args[0]; // csv file to be analyzed
 		String resultPath = args[1]; // the file path where the results are saved.
 		ArrayList<String> lines = Utils.getLines(dataPath, true);
-	
-
+		
+		students = new HashMap<String,Student>();
 		students = loadStudentCourseRecords(lines);
 		
 		// To sort HashMap entries by key values so that we can save the results by student ids in ascending order.
-		//Map<String, Student> sortedStudents = new TreeMap<String,Student>(students); 
+		Map<String, Student> sortedStudents = new TreeMap<String,Student>(students); 
 		
 		// Generate result lines to be saved.
-		//ArrayList<String> linesToBeSaved = countNumberOfCoursesTakenInEachSemester(sortedStudents);
+		ArrayList<String> linesToBeSaved = countNumberOfCoursesTakenInEachSemester(sortedStudents);
 		
 		// Write a file (named like the value of resultPath) with linesTobeSaved.
-		Utils.writeAFile(lines, resultPath);
+		Utils.writeAFile(linesToBeSaved, resultPath);
 	}
-
 	
 	/**
 	 * This method create HashMap<String,Student> from the data csv file. Key is a student id and the corresponding object is an instance of Student.
@@ -58,23 +53,31 @@ public class HGUCoursePatternAnalyzer
 	 * @param lines
 	 * @return
 	 */
-	private HashMap<String,Student> loadStudentCourseRecords(ArrayList<String> lines) 
-	{
-		int a=0;
-		ArrayList<Course> course = new ArrayList<Course>();
+	private HashMap<String,Student> loadStudentCourseRecords(ArrayList<String> lines) {
 		
-		for (String s : lines)
-		{
-			course.add(new Course(s));
-		}
+		// TODO: Implement this method
 		
-		for (a=0; a<course.size(); a++)
-		{
-
-		}
-		return students;
+	      for(String stringFromLine : lines) {
+	         Course CourseToAdd = new Course(stringFromLine);
+	         String studentIdToCheck = CourseToAdd.getstudentId();
+	         
+	         //if the student Id already exists in HashMap
+	         if(!students.containsKey(studentIdToCheck)) {
+	        	 Student studentToAdd = new Student(studentIdToCheck);
+	        	 studentToAdd.addCourse(CourseToAdd);
+	        	 students.put(studentIdToCheck, studentToAdd);
+	         }
+	         //if there is no same thing in HashMap 
+	         else {
+	        	 Student studentInstanceToAddCourse = students.get(studentIdToCheck);
+	        	 studentInstanceToAddCourse.addCourse(CourseToAdd);
+	        	 students.put(studentIdToCheck, studentInstanceToAddCourse);
+	         }
+	      }
 		
+		return students; // do not forget to return a proper variable.
 	}
+
 	/**
 	 * This method generate the number of courses taken by a student in each semester. The result file look like this:
 	 * StudentID, TotalNumberOfSemestersRegistered, Semester, NumCoursesTakenInTheSemester
@@ -82,18 +85,32 @@ public class HGUCoursePatternAnalyzer
      * 0001,14,2,8
 	 * ....
 	 * 
-	 * 0001,14,1,9 => this means, 0001 student registered 14 semeters in total. In the first semeter (1), the student took 9 courses.
+	 * 0001,14,1,9 => this means, 0001 student registered 14 semesters in total. In the first semester (1), the student took 9 courses.
 	 * 
 	 * 
 	 * @param sortedStudents
 	 * @return
 	 */
-	private ArrayList<String> countNumberOfCoursesTakenInEachSemester(Map<String, Student> sortedStudents)
-	{
+	private ArrayList<String> countNumberOfCoursesTakenInEachSemester(Map<String, Student> sortedStudents) {
 		
-		// TODO: Implement this method
+		// TODO: Implement this method 
+		ArrayList<String> numberOfCoursesTakenInEachSemester = new ArrayList<String>();
+		int totalSemester;
 		
-		return null; // do not forget to return a proper variable.
+		for(int  i = 0; i < sortedStudents.size(); i++) {
+			String studentIdToCheck = String.format("%04d", i+1);
+			Student studentToCheck = sortedStudents.get(studentIdToCheck);
+			String studentId = studentToCheck.getStudentId();
+			totalSemester = studentToCheck.getSemesterByYearAndSemester().size();
+			
+			for(int j = 0; j < studentToCheck.getSemesterByYearAndSemester().size(); j++) {
+				int numOfCoursesInNthSemester = studentToCheck.getNumCourseNthSemester(j+1);
+				String studentIdToAdd = studentId;
+				String toAdd = studentIdToAdd + "," + totalSemester + "," + j + "," + numOfCoursesInNthSemester;
+				numberOfCoursesTakenInEachSemester.add(toAdd);
+			}
+		}
+		return numberOfCoursesTakenInEachSemester; // do not forget to return a proper variable
 	}
 }
 
--- src/main/java/edu/handong/analysise/utils/Utils.java
+++ src/main/java/edu/handong/analysise/utils/Utils.java
@@ -1,52 +1,50 @@
 package edu.handong.analysise.utils;
 
 import java.util.ArrayList;
-import 	java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 public class Utils {
 	
-	public static ArrayList<String> getLines(String file,boolean removeHeader)
-	{
-		ArrayList<String> aline =new ArrayList<String>();
+	public static ArrayList<String> getLines(String file, boolean removeHeader){	
+		ArrayList<String> temp = new ArrayList<String>();
+		Scanner inputStream = null;
 		try {
+		
+			File fileCheck = new File(file);
+			if(fileCheck.exists() == false) {
+				throw new NotEnoughArgumentException("The file path does not exist. Please check your CLI argument!");
+			}
+			inputStream = new Scanner(new File(file));
 			
-			Scanner inputStream =new Scanner(new File(file));
-			String line;
-            while(inputStream.hasNextLine())
-            {
-            		line=inputStream.nextLine();
-            		if(removeHeader==true)
-            		{
-            			removeHeader=false;
-            			continue;
-            		}
-            		else
-            		{
-            			aline.add(line);
-            		}	
-            }
-            inputStream.close();
-		} catch(FileNotFoundException e) {
-			System.out.println("The file path does not exist. Please check your CLI argument!");
+			while (inputStream.hasNextLine ()) {
+				String line = inputStream.nextLine();
+				temp.add(line);		
+			}
+			if(removeHeader == true) temp.remove(0);
+		}  catch (FileNotFoundException e) {
+			System.out.println ("Error opening the file " + file);
+			System.exit (0);
+		}catch(NotEnoughArgumentException e) {
+			System.out.println(e);
 		}
-		return aline;
+		inputStream.close();
+		return temp;
 	}
 	
-	public static void writeAFile(ArrayList<String>lines,String targetFileName)
-	{
+	public static void writeAFile(ArrayList<String> lines, String targetFileName) {
 		PrintWriter outputStream = null;
-		
 		try {
 			outputStream = new PrintWriter(targetFileName);
 		} catch(FileNotFoundException e) {
-			System.out.println("The file path does not exist. Please check your CLI argument!");
+			System.out.println("Error opening the file " + targetFileName);
 			System.exit(0);
 		}
-		int i;
-		for(i=0;i<lines.size();i++)
-		{
-			outputStream.println(lines.get(i));
-		}
+        for (String line : lines) {
+            outputStream.println (line);
+        }
+        outputStream.close();
 	}
 }
